b50f8f106af0fbba12b7cbbdcca08fc883c43ce8
Ahmad-script <75048922+Ahmad-script@users.noreply.github.com>
2020-12-04 17:29:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -224,8 +224,15 @@ conn.sendMessage(id, 'Waalaikumsalam, ada yang bisa saya bantu? kalo bingung ket
 else if (text == 'salam'){
 conn.sendMessage(id, 'Waalaikumsalam, ada yang bisa saya bantu? kalo bingung ketik #help ya say..' ,MessageType.text);
 }
-else if (text == 'asalamualaikum'){
-conn.sendMessage(id, 'Waalaikumsalam, ada yang bisa saya bantu? kalo bingung ketik #help ya say..' ,MessageType.text);
+else if (text == '#intro'){
+conn.sendMessage(id, 'INTRO:v',
+'Nama:',
+'Umur:',
+'Kelas:',
+'Punya Pacar Ato gak:v?:',
+'Nama Pacar:v:',
+'Udh Pernah Ngewe:v?:',
+'Jenis Kelamin:v:' ,MessageType.text);
 }
 else if (text == 'Assalamualaikum'){
 conn.sendMessage(id, 'Waalaikumsalam, ada yang bisa saya bantu? kalo bingung ketik #help ya say..' ,MessageType.text);
