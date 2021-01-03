2ec93869519ac1f547101bbfc74575e920e66e4c
Ahmad-script <75048922+Ahmad-script@users.noreply.github.com>
2021-01-03 20:45:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ const content = JSON.stringify(mek.message)
 const from = mek.key.remoteJid
 const type = Object.keys(mek.message)[0]
 const apiKey = 'SLpvUgOcMYwIx0pFeELt'
-const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, product } = MessageType
+const { text, extendedText, contact, location, liveLocation, image, video, sticker, document } = MessageType
 const time = moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')
 body = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text : ''
 budy = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
